eff7269255613c8d1cc91e25d3a38e8c1fccd6b3
Fabrizio Piermarocchi <87669776+Piermaa@users.noreply.github.com>
2021-09-10 18:56:25 -0300

    Add files via upload

---
+++ Create With Code Challenge 2 Solution 2.0/PlayerControllerX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float counter;
+    private float cooldown = 0.3f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+
+        if (counter>=cooldown) 
+        {
+
+
+            // On spacebar press, send dog
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                counter = 0;
+            }
+            
+        }
+    }
+}
---
+++ Create With Code Challenge 2 Solution 2.0/SpawnManagerX.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 2.5f;
+    public int pelotarandom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+
+        // Generate random ball index and random spawn position
+        
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        
+        //Elegir una pelota de forma aleatoria
+        pelotarandom = Random.Range(0, ballPrefabs.Length);
+        
+        // instantiate ball at random spawn location
+        
+        Instantiate(ballPrefabs[pelotarandom], spawnPos, ballPrefabs[pelotarandom].transform.rotation);
+    }
+    private float counter;
+    private float nexW8Time = 5;
+    private void Update()
+    {
+        counter += Time.deltaTime;
+        if (counter >= nexW8Time) 
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+            Debug.LogFormat("Pasaron {0} segundos", nexW8Time);
+            Instantiate(ballPrefabs[pelotarandom], spawnPos, ballPrefabs[pelotarandom].transform.rotation);
+            counter = 0;
+            nexW8Time = Random.Range(1,4);
+
+        }
+    }
+
+}
